f73ab064dc4e628c0b5f812d8e01bf972738d28a
hiranwj <hiranwj.connect@gmail.com>
2024-12-16 13:17:51 +0530

    Add styles to views

--- src/main/java/hiranwj/com/socialbuddy/AppInitializer.java
+++ src/main/java/hiranwj/com/socialbuddy/AppInitializer.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.net.URL;
@@ -17,12 +18,14 @@ public class AppInitializer extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
 
-        URL resource = this.getClass().getResource("view/WelcomeForm.fxml");
+        URL resource = this.getClass().getResource("view/LoginForm.fxml");
 
         Parent container = FXMLLoader.load(resource);
         var scene = new Scene(container);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Table Demo");
+        primaryStage.setTitle("Welcome");
+//        primaryStage.initStyle(StageStyle.UTILITY); // Optionally use StageStyle.UTILITY or StageStyle.TRANSPARENT
+//        primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.show();
         primaryStage.centerOnScreen();
     }
